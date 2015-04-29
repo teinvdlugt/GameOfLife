9376c1f962038c7e2d52c9e620370e3028b672e9
Tein van der Lugt <teinvdlugt@gmail.com>
2015-04-29 21:21:02 +0200

    Add 2d-array for game field and "drawBlocks" method

--- app/src/main/java/com/teinproductions/tein/gameoflife/ViewOfLife.java
+++ app/src/main/java/com/teinproductions/tein/gameoflife/ViewOfLife.java
@@ -4,28 +4,52 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class ViewOfLife extends View {
 
-    private int pixelsPerCell = 30;
+    private int pixelsPerCell = 50;
+
+    private int verCells = 10;
+    private int horCells = 10;
+    private boolean[][] field = new boolean[horCells][verCells];
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+        drawBlocks(canvas);
+        drawGrid(canvas);
+    }
 
+    private void drawBlocks(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setColor(getResources().getColor(R.color.block_color));
+
+        for (int y = 0; y < field.length; y++) {
+            for (int x = 0; x < field[0].length; x++) {
+                if (field[y][x]) {
+                    int left = y * pixelsPerCell;
+                    int right = (y + 1) * pixelsPerCell;
+                    int top = x * pixelsPerCell;
+                    int bottom = (x + 1) * pixelsPerCell;
+
+                    canvas.drawRect(left, top, right, bottom, paint);
+                }
+            }
+        }
+    }
+
+    private void drawGrid(Canvas canvas) {
         Paint paint = new Paint();
         paint.setColor(getResources().getColor(android.R.color.black));
 
-
-        int verCells = (int) Math.floor(getHeight() / 30);
-        int horCells = (int) Math.floor(getWidth() / 30);
-
+        // Vertical lines
         for (int x = 0; x < getWidth(); x += pixelsPerCell) {
             int stopY = verCells * pixelsPerCell;
             canvas.drawLine(x, 0, x, stopY, paint);
         }
 
+        // Horizontal lines
         for (int y = 0; y < getHeight(); y += pixelsPerCell) {
             int stopX = horCells * pixelsPerCell;
             canvas.drawLine(0, y, stopX, y, paint);
@@ -33,8 +57,44 @@ public class ViewOfLife extends View {
     }
 
     @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    protected void onSizeChanged(int w, int h, int oldW, int oldH) {
+        super.onSizeChanged(w, h, oldW, oldH);
+
+        verCells = (int) Math.floor(getHeight() / pixelsPerCell);
+        horCells = (int) Math.floor(getWidth() / pixelsPerCell);
+
+        // Otherwise, the last lines from the grid won't be drawn:
+        if (getHeight() % pixelsPerCell == 0) verCells--;
+        if (getWidth() % pixelsPerCell == 0) horCells--;
+
+        field = new boolean[horCells][verCells];
+
+        generateRandomField();
+
+        Log.d("lollipop", "getWidth(): " + getWidth());
+        Log.d("lollipop", "getHeight(): " + getHeight());
+        Log.d("lollipop", "verCells: " + verCells);
+        Log.d("lollipop", "horCells: " + horCells);
+    }
+
+    /**
+     * For debugging
+     */
+    private void generateRandomField() {
+        for (int y = 0; y < field.length; y++) {
+            for (int x = 0; x < field[0].length; x++) {
+                field[y][x] = (int) Math.floor(Math.random() * 2) == 0;
+            }
+        }
+    }
+
+    public int getPixelsPerCell() {
+        return pixelsPerCell;
+    }
+
+    public void setPixelsPerCell(int pixelsPerCell) {
+        this.pixelsPerCell = pixelsPerCell;
+        invalidate();
     }
 
 
