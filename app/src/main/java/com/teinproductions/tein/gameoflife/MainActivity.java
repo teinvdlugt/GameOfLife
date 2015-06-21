f0f55d91a18da1319add4ffe0ad4bf02076aca8e
Tein van der Lugt <teinvdlugt@gmail.com>
2015-06-21 19:47:14 +0200

    improved autozoom image imagebutton selected background

--- app/src/main/java/com/teinproductions/tein/gameoflife/MainActivity.java
+++ app/src/main/java/com/teinproductions/tein/gameoflife/MainActivity.java
@@ -1,7 +1,6 @@
 package com.teinproductions.tein.gameoflife;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -16,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ViewOfLife viewOfLife;
     private ImageButton playPauseButton, autoZoomButton, clearButton;
+    private ImageButton pencilButton, eraseButton, zoomInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
         playPauseButton = (ImageButton) findViewById(R.id.playPause_button);
         autoZoomButton = (ImageButton) findViewById(R.id.auto_zoom_button);
         clearButton = (ImageButton) findViewById(R.id.clear_button);
+        pencilButton = (ImageButton) findViewById(R.id.pencilButton);
+        eraseButton = (ImageButton) findViewById(R.id.eraseButton);
+        zoomInButton = (ImageButton) findViewById(R.id.zoomInButton);
+
         clearButton.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
@@ -35,19 +39,36 @@ public class MainActivity extends AppCompatActivity {
         });
 
         resetSpeed();
+        viewOfLife.setEditMode(ViewOfLife.EditMode.ADD);
+        pencilButton.setSelected(true);
+        viewOfLife.setAutoZoom(true);
+        autoZoomButton.setSelected(true);
     }
 
     public void onClickPencil(View view) {
         viewOfLife.setEditMode(ViewOfLife.EditMode.ADD);
+        pencilButton.setSelected(true);
+        eraseButton.setSelected(false);
+        zoomInButton.setSelected(false);
     }
 
     public void onClickErase(View view) {
         viewOfLife.setEditMode(ViewOfLife.EditMode.REMOVE);
+        pencilButton.setSelected(false);
+        eraseButton.setSelected(true);
+        zoomInButton.setSelected(false);
+    }
+
+    public void onClickZoomIn(View view) {
+        viewOfLife.setEditMode(ViewOfLife.EditMode.ZOOM_IN);
+        pencilButton.setSelected(false);
+        eraseButton.setSelected(false);
+        zoomInButton.setSelected(true);
     }
 
     public void clear() {
-        viewOfLife.clear();
         viewOfLife.stop();
+        viewOfLife.clear();
         playPauseButton.setImageResource(R.mipmap.ic_play_arrow_black_36dp);
     }
 
@@ -60,7 +81,6 @@ public class MainActivity extends AppCompatActivity {
             viewOfLife.stop();
             playPauseButton.setImageResource(R.mipmap.ic_play_arrow_black_36dp);
         } else {
-            //resetSpeed();
             viewOfLife.start();
             playPauseButton.setImageResource(R.mipmap.ic_pause_black_36dp);
         }
@@ -75,17 +95,13 @@ public class MainActivity extends AppCompatActivity {
         Log.d("sizes", "height: " + viewOfLife.getFieldHeight() + ", width: " + viewOfLife.getFieldWidth());
     }
 
-    public void onClickZoomIn(View view) {
-        viewOfLife.setEditMode(ViewOfLife.EditMode.ZOOM_IN);
-    }
-
     public void onClickAutoZoom(View view) {
         if (viewOfLife.isAutoZoom()) {
             viewOfLife.setAutoZoom(false);
-            autoZoomButton.setBackgroundResource(Color.argb(0, 0, 0, 0));
+            autoZoomButton.setSelected(false);
         } else {
             viewOfLife.setAutoZoom(true);
-            autoZoomButton.setBackgroundColor(getResources().getColor(R.color.block_color));
+            autoZoomButton.setSelected(true);
         }
     }
 
