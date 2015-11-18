c4de241b299355fc18fad6af2f1484e310baa0ea
Tein van der Lugt <teinvdlugt@gmail.com>
2015-11-18 21:10:43 +0100

    Update MainActivity.java to new ViewOfLife2

--- app/src/main/java/com/teinproductions/tein/gameoflife/MainActivity.java
+++ app/src/main/java/com/teinproductions/tein/gameoflife/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
@@ -15,7 +16,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements ViewOfLife.ActivityInterface {
 
-    private ViewOfLife viewOfLife;
+    private ViewOfLife2 viewOfLife;
     private ImageButton playPauseButton, clearButton;
     private ImageButton pencilButton, eraseButton;
     private ImageButton nextGenButton, initialStateButton;
@@ -26,20 +27,16 @@ public class MainActivity extends AppCompatActivity implements ViewOfLife.Activi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*viewOfLife = (ViewOfLife) findViewById(R.id.view_of_life);
+        viewOfLife = (ViewOfLife2) findViewById(R.id.view_of_life);
         playPauseButton = (ImageButton) findViewById(R.id.playPause_button);
-        autoZoomButton = (ImageButton) findViewById(R.id.auto_zoom_button);
         clearButton = (ImageButton) findViewById(R.id.clear_button);
         pencilButton = (ImageButton) findViewById(R.id.pencilButton);
         eraseButton = (ImageButton) findViewById(R.id.eraseButton);
-        zoomInButton = (ImageButton) findViewById(R.id.zoomInButton);
-        zoomOutButton = (ImageButton) findViewById(R.id.zoomOutButton);
-        zoomFitButton = (ImageButton) findViewById(R.id.zoomFitButton);
         nextGenButton = (ImageButton) findViewById(R.id.nextGenButton);
         initialStateButton = (ImageButton) findViewById(R.id.initialStateButton);
         generationTV = (TextView) findViewById(R.id.generation_textView);
 
-        viewOfLife.setActivityInterface(this);
+        /*viewOfLife.setActivityInterface(this);
         clearButton.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
@@ -68,42 +65,42 @@ public class MainActivity extends AppCompatActivity implements ViewOfLife.Activi
     }
 
     public void onClickPencil(View view) {
-        viewOfLife.setEditMode(ViewOfLife.EditMode.ADD);
+        /*viewOfLife.setEditMode(ViewOfLife.EditMode.ADD);
         pencilButton.setSelected(true);
-        eraseButton.setSelected(false);
+        eraseButton.setSelected(false);*/
     }
 
     public void onClickErase(View view) {
-        viewOfLife.setEditMode(ViewOfLife.EditMode.REMOVE);
+        /*viewOfLife.setEditMode(ViewOfLife.EditMode.REMOVE);
         pencilButton.setSelected(false);
-        eraseButton.setSelected(true);
+        eraseButton.setSelected(true);*/
     }
 
     public void onClickZoomIn(View view) {
-        viewOfLife.setEditMode(ViewOfLife.EditMode.ZOOM_IN);
+        /*viewOfLife.setEditMode(ViewOfLife.EditMode.ZOOM_IN);
         pencilButton.setSelected(false);
-        eraseButton.setSelected(false);
+        eraseButton.setSelected(false);*/
     }
 
     public void clear() {
-        viewOfLife.stop();
+        /*viewOfLife.stop();
         viewOfLife.clear();
         playPauseButton.setImageResource(R.mipmap.ic_play_arrow_black_36dp);
-        lockScreenOrientation(false);
+        lockScreenOrientation(false);*/
     }
 
     public void onClickClear(View view) {
-        Toast.makeText(this, getString(R.string.short_click_clear_message), Toast.LENGTH_SHORT).show();
+        Snackbar.make(viewOfLife, getString(R.string.short_click_clear_message), Snackbar.LENGTH_SHORT).show();
     }
 
     public void onClickPlayPause(View view) {
-        if (viewOfLife.isRunning()) {
+        /*if (viewOfLifer.isRunning()) {
             viewOfLife.stop();
             playPauseButton.setImageResource(R.mipmap.ic_play_arrow_black_36dp);
         } else {
             viewOfLife.start();
             playPauseButton.setImageResource(R.mipmap.ic_pause_black_36dp);
-        }
+        }*/
     }
 
     public void onClickSettings(View view) {
@@ -129,14 +126,14 @@ public class MainActivity extends AppCompatActivity implements ViewOfLife.Activi
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-        /*if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
             viewOfLife.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        }*/
+        }
     }
 
     @Override
@@ -164,28 +161,28 @@ public class MainActivity extends AppCompatActivity implements ViewOfLife.Activi
     }
 
     private void resetStrokeWidth() {
-        String speed = PreferenceManager.getDefaultSharedPreferences(this)
+        /*String speed = PreferenceManager.getDefaultSharedPreferences(this)
                 .getString(getString(R.string.stroke_width_key), "1");
 
         try {
             viewOfLife.setStrokeWidth(Integer.parseInt(speed));
         } catch (NumberFormatException e) {
             viewOfLife.setStrokeWidth(1);
-        }
+        }*/
     }
 
     public void onClickNextGen(View view) {
-        new Thread(new Runnable() {
+        /*new Thread(new Runnable() {
             @Override
             public void run() {
                 viewOfLife.nextGeneration();
                 viewOfLife.postInvalidate();
             }
-        }).start();
+        }).start();*/
     }
 
     public void onClickInitialState(View view) {
-        viewOfLife.restoreInitialState();
+        /*viewOfLife.restoreInitialState();*/
     }
 
     @Override
