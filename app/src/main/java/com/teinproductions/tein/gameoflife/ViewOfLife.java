23a4c4a842b1b5f293f27f8114e0e6f7729bfc4e
Tein van der Lugt <teinvdlugt@gmail.com>
2015-12-03 17:10:30 +0100

    Remove redundant ViewOfLife methods

--- app/src/main/java/com/teinproductions/tein/gameoflife/ViewOfLife.java
+++ app/src/main/java/com/teinproductions/tein/gameoflife/ViewOfLife.java
@@ -457,43 +457,9 @@ public class ViewOfLife extends View {
     public void load(Life life) {
         running = false;
         cells = life.getCells();
-        /*checkCellsDocumented();
-        recountNeighbours();*/
         updateGen0();
         startX = startY = 0;
         invalidate();
-    }
-
-    private void checkCellsDocumented() {
-        int numOfCells = cells.size();
-        for (int i = 0; i < numOfCells; i++) {
-            checkNeighboursDocumented(cells.get(i)[0], cells.get(i)[1]);
-        }
-    }
-
-    private void checkNeighboursDocumented(short x, short y) {
-        for (short[] neighbour : new short[][]{{(short) (x - 1), (short) (y - 1)}, {x, (short) (y - 1)},
-                {(short) (x + 1), (short) (y - 1)}, {(short) (x - 1), y}, {(short) (x + 1), y},
-                {(short) (x - 1), (short) (y + 1)}, {x, (short) (y + 1)}, {(short) (x + 1), (short) (y + 1)}}) {
-            checkCellDocumented(neighbour[0], neighbour[1]);
-        }
-    }
-
-    private void checkCellDocumented(short x, short y) {
-        for (short[] cell : cells) {
-            if (cell[0] == x && cell[1] == y) {
-                return;
-            }
-        }
-
-        cells.add(new short[]{x, y, 0, 0});
-    }
-
-    private void recountNeighbours() {
-        for (int i = 0; i < cells.size(); i++) {
-            short[] cell = cells.get(i);
-            cells.set(i, new short[]{cell[0], cell[1], cell[2], neighbours(cell[0], cell[1])});
-        }
     }
 
     public void setCellColor(@ColorInt int color) {
